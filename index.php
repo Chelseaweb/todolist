eefc93eb0fd684682092f737ec425e7e48e329ee
todolist <chelsea@gmail.com>
2020-05-27 08:47:59 -0700

    changes

--- index.php
+++ index.php
@@ -3,84 +3,29 @@
 <html>
 <head>
 	<title>ToDo List Application PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel= "stylesheet" text="text/css" href="style.css">
 </head>
+
 <body>
-  <style>
-    body{
-      background:rgb(210, 210, 236);
-    }
-    </style>
-
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-      <li data-target="#myCarousel" data-slide-to="4"></li>
-      <li data-target="#myCarousel" data-slide-to="5"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="images/holiday2.jpg." alt="Survive" style="width:400px;">
-      </div>
-
-      <div class="item">
-        <img src="images/holiday4.jpg." alt="Survive" style="width:400px;">
-      </div>
-
-
-      
-    
-      <div class="item">
-        <img src="images/holiday5.jpg." alt="Survive" style="width:400px">
-      </div>
-
-      <div class="item">
-        <img src="images/holiday6.jpg." alt="Survive" style="width:400px;">
-      </div>
-
-      <div class="item">
-        <img src="images/holiday7.jpg." alt="Survive" style="width:400px;">
-      </div>
-
-      <div class="item">
-        <img src="images/holiday8.jpg." alt="Survive" style="width:400px;">
-      </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
+ 
+<div class="block"> 
   
-	<div class="heading">
-		<h2 style="font-style: 'Hervetica';">Plan,organise and list your holiday todos</h2>
+	<div id="image">
+		<img src="images/holiday5.jpg" alt="thailand" width="300px">
 	</div>
+
+	<div class="heading">
+		<h2 style="font-style: 'Hervetica';">Plan,organise and list your holiday to do's.</h2>
+	</div>
+
 	<form method="post" action="index.php" class="input_form">
 		<input type="text" name="task" class="task_input">
     <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>   
   </form>
- </body>
- </html>
-  
+
+     </body>
+        </html>
+ 
 
   <?php if (isset($errors)) { ?>
 	<p><?php echo $errors; ?></p>
@@ -131,7 +76,7 @@ if (isset($_POST['del_task'])) {
 		<tr>
 			<th>N</th>
 			<th>Tasks</th>
-			<th style="width: 60px;">Action</th>
+			<th style="width: 70px;">Action</th>
 		</tr>
 	</thead>
 
@@ -150,6 +95,8 @@ if (isset($_POST['del_task'])) {
 		<?php $i++; } ?>	
 	</tbody>
 </table>
+
+
 
   
 
